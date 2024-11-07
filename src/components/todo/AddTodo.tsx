dfecce89e1019e5ec40cee85e40326a416e35604
Mehedi Hasan <thecodermehedi@gmail.com>
2024-11-07 08:28:43 +0600

    refactor: update AddTodo component to improve task addition functionality and UI; integrate TodoForm and enhance form handling

--- src/components/todo/AddTodo.tsx
+++ src/components/todo/AddTodo.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
@@ -24,25 +23,29 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useMediaQuery } from "@/hooks/useMediaQuery"
 import { PlusIcon } from "lucide-react"
+import { useAppDispatch } from "@/redux/hook"
+import { addTodo } from "@/redux/features/todoSlice"
 
-export function AddTodo() {
- const [open, setOpen] = React.useState(false)
+const AddTodo = () => {
  const isDesktop = useMediaQuery("(min-width: 768px)")
+ const [open, setOpen] = React.useState(false)
+
+ const handleClose = () => setOpen(false);
 
  if (isDesktop) {
   return (
    <Dialog open={open} onOpenChange={setOpen}>
     <DialogTrigger asChild>
-     <Button className="bg-primary-gradient text-white"> <PlusIcon /></Button>
+     <Button className="bg-primary-gradient text-white"> <PlusIcon /> Task</Button>
     </DialogTrigger>
     <DialogContent className="sm:max-w-[425px]">
      <DialogHeader>
-      <DialogTitle>Edit profile</DialogTitle>
+      <DialogTitle>Add Task</DialogTitle>
       <DialogDescription>
-       Make changes to your profile here. Click save when you're done.
+       Add your task here. Click save when you're done.
       </DialogDescription>
      </DialogHeader>
-     <ProfileForm />
+     <TodoForm onClose={handleClose} />
     </DialogContent>
    </Dialog>
   )
@@ -60,7 +63,7 @@ export function AddTodo() {
       Make changes to your profile here. Click save when you're done.
      </DrawerDescription>
     </DrawerHeader>
-    <ProfileForm className="px-4" />
+    <TodoForm className="px-4" onClose={handleClose} />
     <DrawerFooter className="pt-2">
      <DrawerClose asChild>
       <Button variant="outline">Cancel</Button>
@@ -71,18 +74,36 @@ export function AddTodo() {
  )
 }
 
-function ProfileForm({ className }: React.ComponentProps<"form">) {
+type TodoFormProps = {
+ className?: string,
+ onClose: () => void
+}
+
+const TodoForm = ({ className, onClose }: TodoFormProps) => {
+ const dispatch = useAppDispatch();
+ const [task, setTask] = React.useState("")
+ const [description, setDescription] = React.useState("")
+ const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault()
+  console.log({ task, description })
+  if (!task) return alert("Task is required");
+  if (!description) return alert("Description is required");
+  dispatch(addTodo({ title: task, description }))
+  onClose()
+ }
  return (
-  <form className={cn("grid items-start gap-4", className)}>
+  <form className={cn("grid items-start gap-4", className)} onSubmit={handleSubmit}>
    <div className="grid gap-2">
-    <Label htmlFor="email">Email</Label>
-    <Input type="email" id="email" defaultValue="shadcn@example.com" />
+    <Label htmlFor="task">Task</Label>
+    <Input type="text" id="task" onBlur={(e) => setTask(e.target.value)} className="focus-visible:ring-0 bg-gray-100 focus-visible:bg-white" required />
    </div>
    <div className="grid gap-2">
-    <Label htmlFor="username">Username</Label>
-    <Input id="username" defaultValue="@shadcn" />
+    <Label htmlFor="description">Description</Label>
+    <textarea id="description" onBlur={(e) => setDescription(e.target.value)} className="focus-visible:ring-0 bg-gray-100 focus-visible:bg-white resize-none focus:outline-none rounded-lg border p-2 text-sm" rows={7} required />
    </div>
    <Button type="submit">Save changes</Button>
   </form>
  )
 }
+
+export default AddTodo
