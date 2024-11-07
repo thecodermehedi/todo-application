4767fc8f0d4a279ee904263cef2a5b5b405be53f
Mehedi Hasan <thecodermehedi@gmail.com>
2024-11-07 08:28:23 +0600

    refactor: update FilterTodo component to enhance UI and functionality; adjust button styles and filter options based on screen size

--- src/components/todo/FilterTodo.tsx
+++ src/components/todo/FilterTodo.tsx
@@ -1,29 +1,29 @@
 import {
  DropdownMenu,
  DropdownMenuContent,
- DropdownMenuLabel,
  DropdownMenuRadioGroup,
  DropdownMenuRadioItem,
- DropdownMenuSeparator,
  DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "../ui/button";
 import { useState } from "react";
+import { ListFilter } from "lucide-react";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { cn } from "@/lib/utils";
 
 const FilterTodo = () => {
+ const isDesktop = useMediaQuery("(min-width: 768px)")
  const [position, setPosition] = useState("bottom");
  return (
   <DropdownMenu>
    <DropdownMenuTrigger asChild>
-    <Button variant="outline">Filter</Button>
+    <Button variant="outline" className="bg-primary-gradient focus-visible:ring-0 text-white hover:text-white"><ListFilter />{isDesktop && "Priority"}</Button>
    </DropdownMenuTrigger>
-   <DropdownMenuContent className="w-56">
-    <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
-    <DropdownMenuSeparator />
+   <DropdownMenuContent className={cn(isDesktop ? "w-56" : "min-w-fit")}>
     <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
-     <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
-     <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
-     <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+     <DropdownMenuRadioItem value="high">Hight</DropdownMenuRadioItem>
+     <DropdownMenuRadioItem value="medium">Medium</DropdownMenuRadioItem>
+     <DropdownMenuRadioItem value="low">Low</DropdownMenuRadioItem>
     </DropdownMenuRadioGroup>
    </DropdownMenuContent>
   </DropdownMenu>
